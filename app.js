dbd0bd0c67d47bb4ec05b27f9a777b31ee634b41
sagar460 <shresthasagr7000@gmail.com>
2025-05-07 15:39:11 +0545

    new commit

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const express = require("express");
+const { books } = require("./database/connection.js");
 const app = express();
 require("./database/connection.js");
 
-app.get("/books", function (req, res) {
+app.use(express.json());
+
+app.get("/books", async function (req, res) {
   //logic to fetch books from database
+  const datas = await books.findAll();
   res.json({
     message: "Books fetched successfully",
+    datas,
   });
 });
-app.post("/books", function (req, res) {
-  //logic to add new books to database
+app.post("/books", async function (req, res) {
+  //logic to add new books to database goes here..
+  //console.log(req.body)
+  //const bookName=req.body.bookName
+  //const bookPrice=req.body.bookPrice
+  const { bookName, bookPrice, bookAuthor, bookGenre } = req.body;
+  await books.create({
+    bookName,
+    bookPrice,
+    bookAuthor,
+    bookGenre,
+  });
   res.json({
     message: "Books added successfully",
   });
